dc7a7e9d69e289a343a3e0a906975f10ab9bbaae
David Lanzendörfer <leviathan@libresilicon.com>
2023-05-26 02:21:15 +0100

    Cleanup ChatGPTs mess and refeed

--- verilog/src/chatgpt/Matrix_Multiplication.v
+++ verilog/src/chatgpt/Matrix_Multiplication.v
@@ -1,89 +1,70 @@
-`define SEQ_LENGTH 512
-`define NUM_HEADS 6
+`define SEQ_BITS 14
 
 module Matrix_Multiplication (
   input wire         clk,
   input wire         reset,
   input wire         enable,
-  input wire [31:0]  matrixA_in [`SEQ_LENGTH*`SEQ_LENGTH*`NUM_HEADS-1],
-  input wire [31:0]  matrixB_in [0:`SEQ_LENGTH*`NUM_HEADS-1],
-  output reg [31:0]  matrixC_out [`SEQ_LENGTH*`SEQ_LENGTH*`NUM_HEADS-1]
+  input wire [31:0]  operation_reg [6],
+  input wire [31:0]  matrixA_in [`SEQ_BITS:0][`SEQ_BITS:0],
+  input wire [31:0]  matrixB_in [`SEQ_BITS:0][`SEQ_BITS:0],
+  output reg [31:0]  matrixC_out [`SEQ_BITS:0][`SEQ_BITS:0],
+  output reg done
 );
 
-  // Parameters
-  parameter SEQ_LENGTH = `SEQ_LENGTH;
-  parameter NUM_HEADS = `NUM_HEADS;
-  parameter FP_WIDTH = 32;
-
   // Internal registers and wires
-  reg [31:0] a_idx;
-  reg [31:0] b_idx;
-  reg [31:0] c_idx;
-  reg [FP_WIDTH-1:0] matrixA_reg;
-  reg [FP_WIDTH-1:0] matrixB_reg;
-  reg [FP_WIDTH-1:0] matrixC_reg;
-  reg [2:0] state;
-  reg enable_reg;
+  reg integer i;
+  reg integer j;
+  reg integer k;
+  reg integer state;
 
   // State definition
-  localparam IDLE = 3'b000;
-  localparam LOAD_MATRIX_A = 3'b001;
-  localparam LOAD_MATRIX_B = 3'b010;
-  localparam MATRIX_MULT = 3'b011;
+  localparam IDLE = 0;
+  localparam LOOP1 = 1;
+  localparam LOOP2 = 2;
+  localparam LOOP3 = 3;
 
-  // Assign initial state and enable signal
+  /* In C we would do two loops like this:
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < P; j++) {
+      matrixC_out[i][j] = 0;  // Initialize the element in the result matrix
+      for (int k = 0; k < M; k++) {
+        matrixC_out[i][j] += matrixA_in[i][k] * matrixB_in[k][j];
+      }
+    }
+  }
+  But that won't work in Verilog, because for loops work differently,
+  so we've got to implement this for loop as a state machine instead.
+  */
+  // Assign initial state
   always @(posedge clk) begin
     if (reset) begin
       state <= IDLE;
-      enable_reg <= 0;
-    end
-    else begin
-      state <= enable ? (state == MATRIX_MULT ? IDLE : state + 1) : state;
-      enable_reg <= enable;
-    end
-  end
-
-  // Matrix multiplication logic
-  always @(posedge clk) begin
-    case (state)
-      IDLE:
-        // Reset indices and result register
-        if (reset) begin
-          a_idx <= 0;
-          b_idx <= 0;
-          c_idx <= 0;
-          matrixC_reg <= 0;
+      done <= 1;
+      for (int j=0; j< `SEQ_BITS; j++) begin
+        for (int i=0; i< `SEQ_BITS; j++) begin
+          matrixC_out[j][i] <= 0;
         end
-      LOAD_MATRIX_A:
-        // Load matrixA_reg
-        if (enable_reg) begin
-          matrixA_reg <= matrixA_in[a_idx];
-          a_idx <= a_idx + 1;
-        end
-      LOAD_MATRIX_B:
-        // Load matrixB_reg
-        if (enable_reg) begin
-          matrixB_reg <= matrixB_in[b_idx];
-          b_idx <= b_idx + 1;
-        end
-      MATRIX_MULT:
-        // Perform matrix multiplication
-        if (enable_reg) begin
-          matrixC_reg <= matrixC_reg + matrixA_reg * matrixB_reg;
-          c_idx <= c_idx + 1;
-        end
-    endcase
-  end
-
-  // Output
-  always @(posedge clk) begin
-    if (reset) begin
-      matrixC_out <= 0;
-    end
-    else begin
-      if (enable_reg && state == MATRIX_MULT) begin
-        matrixC_out <= matrixC_reg;
       end
+    end
+    else begin
+      case (state)
+        IDLE:
+          if (enable) begin
+            // Reset indices and result register
+            state <= LOOP1;
+            i <= 0;
+            j<= 0;
+            k <= 0;
+            done <= 0;
+          end
+        LOOP1: begin // for (int i = 0; i < N; i++) {
+        end
+        LOOP2: begin // for (int j = 0; j < P; j++) {
+        end
+        LOOP3: begin // or (int k = 0; k < M; k++) {
+          matrixC_out[i][j] <= matrixC_out[i][j] + matrixA_in[i][k] * matrixB_in[k][j];
+        end
+      endcase
     end
   end
 

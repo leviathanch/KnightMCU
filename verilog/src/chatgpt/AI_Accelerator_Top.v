6f2502f0c449d2c8c2a6db8d5402b633d3127a4c
David Lanzendörfer <leviathan@libresilicon.com>
2023-06-04 01:48:00 +0100

    Getting rid of conflicting drivers

--- verilog/src/chatgpt/AI_Accelerator_Top.v
+++ verilog/src/chatgpt/AI_Accelerator_Top.v
@@ -78,6 +78,7 @@ module AI_Accelerator_Top #(
         mem_opdone <= 1;
         mem_read_wait  <= 0;
         sram_en <= 0;
+        sram_we <= 4'b0000;
       end
     end
     else if ( mem_write_wait ) begin
@@ -92,41 +93,56 @@ module AI_Accelerator_Top #(
       mem_opdone <= 0;
     end
     else if (! mem_opdone ) begin
-      case ( operation ) // Register 1 holds the operation to be executed
-        // Enable corresponding module based on operation value in operation register
-        `TYPE_BW'h1: begin // matrix multiplication
-          case (mmul_mem_op)
-            2'b01: begin // Read
-              sram_en <= 1;
-              sram_addr <= mmul_addr_o[`KICP_SRAM_AWIDTH-1:0];
-              mem_read_wait <= 1;
-            end
-            2'b11: begin // Write
-              sram_we <= 4'b1111;
-              sram_en <= 1;
-              sram_addr <= mmul_addr_o[`KICP_SRAM_AWIDTH-1:0];
-              sram_data_i <= mmul_data_o;
-              mem_write_wait <= 1;
-            end
-          endcase
-        end
-        `TYPE_BW'h2: begin // matrix convolution
-          case (mconv_mem_op)
-            2'b01: begin // Read
-              sram_en <= 1;
-              sram_addr <= mconv_addr_o[`KICP_SRAM_AWIDTH-1:0];
-              mem_read_wait <= 1;
-            end
-            2'b11: begin // Write
-              sram_we <= 4'b1111;
-              sram_en <= 1;
-              sram_addr <= mconv_addr_o[`KICP_SRAM_AWIDTH-1:0];
-              sram_data_i <= mconv_data_o;
-              mem_write_wait <= 1;
-            end
-          endcase
-        end
-      endcase
+      if ( wbctrl_mem_op == 2'b01 ) begin // Read
+          sram_we <= 4'b0000;
+          sram_en <= 1;
+          sram_addr <= wbctrl_mem_addr;
+          mem_read_wait <= 1;
+      end
+      else if ( wbctrl_mem_op == 2'b11 ) begin // Write
+          sram_we <= 4'b1111;
+          sram_en <= 1;
+          sram_addr <= wbctrl_mem_addr;
+          sram_data_i <= wbctrl_mem_data;
+          mem_write_wait <= 1;
+      end
+      else begin
+        case ( operation ) // Register 1 holds the operation to be executed
+          // Enable corresponding module based on operation value in operation register
+          `TYPE_BW'h1: begin // matrix multiplication
+            case (mmul_mem_op)
+              2'b01: begin // Read
+                sram_en <= 1;
+                sram_addr <= mmul_addr_o[`KICP_SRAM_AWIDTH-1:0];
+                mem_read_wait <= 1;
+              end
+              2'b11: begin // Write
+                sram_we <= 4'b1111;
+                sram_en <= 1;
+                sram_addr <= mmul_addr_o[`KICP_SRAM_AWIDTH-1:0];
+                sram_data_i <= mmul_data_o;
+                mem_write_wait <= 1;
+              end
+            endcase
+          end
+          `TYPE_BW'h2: begin // matrix convolution
+            case (mconv_mem_op)
+              2'b01: begin // Read
+                sram_en <= 1;
+                sram_addr <= mconv_addr_o[`KICP_SRAM_AWIDTH-1:0];
+                mem_read_wait <= 1;
+              end
+              2'b11: begin // Write
+                sram_we <= 4'b1111;
+                sram_en <= 1;
+                sram_addr <= mconv_addr_o[`KICP_SRAM_AWIDTH-1:0];
+                sram_data_i <= mconv_data_o;
+                mem_write_wait <= 1;
+              end
+            endcase
+          end
+        endcase
+      end
     end
   end
 
@@ -186,24 +202,29 @@ module AI_Accelerator_Top #(
   */
   reg busy;
   reg started;
+  reg finished;
   always @(posedge wb_clk_i) begin
     if (wb_rst_i) begin
       busy <= 1'b0;
       started <= 1'b0;
       multiplier_enable <= 1'b0;
       convolution_enable <= 1'b0;
+      finished <= 0;
     end
     else if ( started && busy ) begin
-      started <= 1'b0;
+      started <= 0;
     end
-    else begin
+    else if ( status == `TYPE_BW'h0000_0000 && finished ) begin
+       finished <= 0;
+    end
+    else if ( !finished ) begin
       case ( operation ) // Register 1 holds the operation to be executed
         // Enable corresponding module based on operation value in operation register
         `TYPE_BW'h1: begin // matrix multiplication
           if( matrix_mult_done && busy ) begin
             busy <= 1'b0;
             multiplier_enable <= 1'b0; // Enable matrix multiplication module
-            status <= `TYPE_BW'h0; // Done
+            finished <= 1; // Done
           end
           else if ( status == `TYPE_BW'hffff_ffff ) begin
             busy <= 1'b1; // indicate that we started operation
@@ -215,7 +236,7 @@ module AI_Accelerator_Top #(
           if( matrix_conv_done && busy ) begin
             busy <= 1'b0;
             convolution_enable <= 1'b0; // Enable matrix multiplication module
-            status <= `TYPE_BW'h0; // Done
+            finished <= 1; // Done
           end
           else if ( status == `TYPE_BW'hffff_ffff ) begin
             busy <= 1'b1; // indicate that we started operation
@@ -232,96 +253,111 @@ module AI_Accelerator_Top #(
     Manages read and write operations from master.
     Implemented by ChatGPT
   */
-  reg read_wait_cycle;
-  reg write_wait_cycle;
-  reg [1:0] wb_state;
+  reg [1:0] wbctrl_mem_op; // Read 01 /Write 11 /None 00
+  reg [31:0] wbctrl_mem_addr;
+  reg [31:0] wbctrl_mem_data;
+  reg [31:0] wbctrl_addr_buf;
+  reg [31:0] wbctrl_data_buf;
+
+  integer wb_state;
+
+  localparam IDLE = 0;
+  localparam WRITE = 1;
+  localparam READ = 2;
+  localparam WAIT_READ = 3;
+  localparam WAIT_READ_DONE = 4;
+  localparam WAIT_WRITE_DONE = 5;
+  localparam READ_DONE = 6;
+  localparam WRITE_DONE = 7;
+
   always @(posedge wb_clk_i or posedge wb_rst_i) begin
     if (wb_rst_i) begin
-      wb_state <= 2'b00;
+      wb_state <= IDLE;
       wb_ack <= 1'b0;
       wb_data_o <= 32'b0;
-      read_wait_cycle <= 0;
-      write_wait_cycle <= 0;
       sram_we <= 4'b0000;
       sram_en <= 0;
+      wbctrl_mem_op <= 2'b00;
+      wbctrl_mem_addr <= 32'b0;
+      wbctrl_mem_data <= 32'b0;
+      wbctrl_addr_buf <= 32'b0;
+      wbctrl_data_buf <= 32'b0;
     end
-    else if (read_wait_cycle) begin
-      if( sram_addr != sram_data_o ) begin
-        read_wait_cycle <= 0;
-        sram_en <= 0;
-        wb_data_o <= sram_data_o;
-      end
-    end
-    else if (write_wait_cycle) begin
-      if( sram_data_i == sram_data_o ) begin
-        sram_we <= 4'b0000;
-        sram_en <= 0;
-        write_wait_cycle <= 0;
-      end
+    else if (finished) begin
+      status <= 0;
     end
     else begin
       case (wb_state)
-        2'b00: begin // Idle state
-          if ( busy ) begin
-            wb_ack <= 1'b0;
-          end
-          else if (wb_cyc_i && wb_stb && !wb_ack) begin
-            wb_ack <= 1'b1;
-            if (wb_we_i) begin
-              wb_data_o <= 32'h0000_0000;
-              wb_state <= 2'b01; // Write state
-            end else begin
-              // increments of 1 become 4 because 32 int32_t = 4 bytes:
-              if((wb_addr_i-ADDR_OFFSET)/4 == 32'h0) begin
-                wb_data_o <= operation;
-              end
-              else if((wb_addr_i-ADDR_OFFSET)/4 == 32'h1 ) begin
-                wb_data_o <= status;
-              end
-              else begin
-                read_wait_cycle <= 1;
-                sram_en <= 1;
-                sram_addr <= wb_addr_i[`KICP_SRAM_AWIDTH+1:0]/4-2;
-              end
-              wb_state <= 2'b10; // Read state
+        IDLE: begin // Idle state
+          wb_ack <= 1'b0;
+          if (wb_cyc_i && wb_stb && !wb_ack) begin
+            wbctrl_addr_buf <= wb_addr_i;
+            wbctrl_data_buf <= wb_data_i;
+            if( (wb_addr_i-ADDR_OFFSET) > 4)
+              wbctrl_mem_addr <= wb_addr_i[`KICP_SRAM_AWIDTH-1:0]/4-2;
+            else
+              wbctrl_mem_addr <= 0;
+            wb_data_o <= 32'h0000_0000;
+            if (wb_we_i) begin // Writing requested
+              wb_state <= WRITE; // Write state
+            end else begin // Reading requested
+              wb_state <= READ; // Read state
             end
           end
         end
-
-        2'b01: begin // Write state
-          if (!wb_cyc_i) begin
-            wb_ack <= 1'b0;
-            wb_state <= 2'b00; // Return to Idle state
-          end else if (!wb_stb) begin
-            wb_ack <= 1'b0;
-          end else if (wb_we_i) begin
-            // increments of 1 become 4 because 32 int32_t = 4 bytes:
-            if((wb_addr_i-ADDR_OFFSET)/4 == 32'h0 ) begin
-              operation <= wb_data_i;
-            end
-            else if((wb_addr_i-ADDR_OFFSET)/4 == 32'h1 ) begin
-              status <= wb_data_i;
-            end
-            else begin
-              sram_we <= 4'b1111;
-              sram_en <= 1;
-              write_wait_cycle <= 1;
-              sram_addr <= wb_addr_i[`KICP_SRAM_AWIDTH+1:0]/4-2;
-              sram_data_i <= wb_data_i; //[`TYPE_BW-1:0];
-            end
-            wb_ack <= 1'b1;
+        READ: begin // Read state
+          // increments of 1 become 4 because 32 int32_t = 4 bytes:
+          if( (wbctrl_addr_buf-ADDR_OFFSET) == 0) begin
+            wb_data_o <= operation;
+            wb_state <= READ_DONE; // Read done
+          end
+          else if( (wbctrl_addr_buf-ADDR_OFFSET) == 4 ) begin
+            wb_data_o <= status;
+            wb_state <= READ_DONE; // Read done
+          end
+          else begin
+            wbctrl_mem_op <= 2'b01;
+            wb_state <= WAIT_READ_DONE; // Read state
           end
         end
-
-        2'b10: begin // Read state
-          if (!wb_cyc_i) begin
-            wb_ack <= 1'b0;
-            wb_state <= 2'b00; // Return to Idle state
-          end else if (!wb_stb) begin
-            wb_ack <= 1'b0;
+        WRITE: begin // Write state
+          // increments of 1 become 4 because 32 int32_t = 4 bytes:
+          //$display("Writing %d to %x (%x)", $signed(wbctrl_data_buf), (wbctrl_addr_buf-ADDR_OFFSET), wbctrl_addr_buf);
+          if( (wbctrl_addr_buf-ADDR_OFFSET) == 0 ) begin
+            operation <= wbctrl_data_buf;
+            wb_state <= WRITE_DONE; // Write done
+          end
+          else if( (wbctrl_addr_buf-ADDR_OFFSET) == 4 ) begin
+            status <= wbctrl_data_buf;
+            wb_state <= WRITE_DONE; // Write done
+          end
+          else begin
+            wbctrl_mem_op <= 2'b11;
+            wbctrl_mem_data <= wbctrl_data_buf;
+            wb_state <= WAIT_WRITE_DONE; // Wait for write finished
           end
         end
-
+        WAIT_READ_DONE: begin // Wait for reading done
+          if ( mem_opdone ) begin
+            wb_data_o <= sram_data_o;
+            wbctrl_mem_op <= 2'b00;
+            wb_state <= READ_DONE; // Go to read done
+          end
+        end
+        READ_DONE: begin // Read done
+          wb_ack <= 1'b1;
+          wb_state <= IDLE; // Return to Idle stat
+        end
+        WAIT_WRITE_DONE: begin // Wait write for done
+          if ( mem_opdone ) begin
+            wbctrl_mem_op <= 2'b00;
+            wb_state <= WRITE_DONE; // Write done
+          end
+        end
+        WRITE_DONE: begin // Wait write for done
+          wb_ack <= 1'b1;
+          wb_state <= IDLE; // Return to Idle state
+        end
       endcase
     end
   end
